8da67782e10a9bb358ef6bd1e4a6bb3114114ab9
applecat112 <minhhuy112ishere@gmail.com>
2026-05-20 23:42:45 +0700

    fix mysql timeout

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,29 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "portfolio"
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("MySQL connection error:", err);
+    return;
+  }
 
-db.connect((err) => {
-    if(err) {
-        console.log("Database Error:", err);
-    }else {
-        console.log("MySQL Connected");
-    }
+  console.log("MySQL Connected");
+
+  connection.release();
 });
 
-module.exports = db;
+module.exports = pool.promise();
